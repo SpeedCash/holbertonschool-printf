07a4e6bc6244fc19b9eff45904f44081e3a378e2
SpeedCash <tcravero@gmail.com>
2023-11-23 12:56:05 +0000

    get function updated

--- get_function.c
+++ get_function.c
@@ -1,20 +1,15 @@
 #include "main.h"
-#include <stddef.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
 /**
 * get_function - function specifiers.
 *
 * @con_spec: Conversion specifiers.
-* @args: arguments.
-*
-* Return: char count.
+* Return: Pointer to the function corresponding to the specifier.
 */
-
 int (*get_function(const char *con_spec))(va_list)
 {
-    int i = 0;
-    int count_x = 0;
-
     op_t spec[] = {
         {'c', format_char},
         {'s', format_string},
@@ -24,18 +19,12 @@ int (*get_function(const char *con_spec))(va_list)
         {0, NULL}
     };
 
-    while (spec[i].specifiers)
-    {
-        if (*con_spec == *spec[i].f)
-            count_x += spec[i].f(args);
-        i++;
-    }
+    int i;
+    for (i = 0; spec[i].op != 0; i++)
+        {
+        if (*con_spec == spec[i].op)
+            return spec[i].f;
+        }
 
-    if (count_x == 0)
-    {
-        count_x += _putchar('%');
-        count_x += _putchar(con_spec);
-    }
-
-    return count_x;
+    return NULL;
 }
