57c08558403fd522fb7a5817b6ae31392d44cb58
SpeedCash <tcravero@gmail.com>
2023-11-23 10:10:48 +0000

    update function

--- ap_function.c
+++ ap_function.c
@@ -10,33 +10,42 @@
 * Return: char count.
 */
 
+int format_char(va_list args);
+int format_string(va_list args);
+int format_mod(va_list args);
+int format_int(va_list args);
+typedef struct
+{
+    char specifiers;
+    int (*f)(va_list);
+} specifiers_t;
+
 int get_function(char con_spec, va_list args)
 {
-	int i = 0;
+    int i = 0;
+    int count_x = 0;
 
-	int count_x = 0;
+    specifiers_t spec[] = {
+        {'c', format_char},
+        {'s', format_string},
+        {'%', format_mod},
+        {'d', format_int},
+        {'i', format_int},
+        {0, NULL}
+    };
 
-	specifiers_t spec[] = {
-		{'c', format_char};
-		{'s', format_string};
-		{'%', format_mod};
-		{'d', format_int};
-		{'i', format_int};
-		{0, NULL};
-	};
+    while (spec[i].specifiers)
+    {
+        if (con_spec == spec[i].specifiers)
+            count_x += spec[i].f(args);
+        i++;
+    }
 
-	while (spec[i].specifiers)
-	{
-		if (con_spec == spec[i].specifiers)
-			count_x += spec[i].f(args);
-		i++;
-	}
+    if (count_x == 0)
+    {
+        count_x += _putchar('%');
+        count_x += _putchar(con_spec);
+    }
 
-	if (count_x == 0)
-	{
-		count_x += _putchar('%');
-		count_x += _putchar(con_spec);
-	}
-
-	return (count_x);
+    return count_x;
 }
