061cca97ca9a9d7ed0fee6ded43db7171ea1df89
SpeedCash <tcravero@gmail.com>
2023-11-23 12:06:36 +0000

    updated main.h

--- main.h
+++ main.h
@@ -2,21 +2,22 @@
 #define MAIN_H
 #include <stdarg.h>
 
-/*Prototypes*/
-
 typedef struct op
 {
 	char *op;
-	int (*f)(int a, int b);
+	int (*f)(va_list args);
 } op_t;
 
 
 int _putchar(char c);
 int _printf(const char *format, ...);
 int format_char(va_list args);
-void printf_int(const char *format, ...);
 int format_string(va_list args);
 int format_mod(va_list args);
+int (*get_function(const char *con_spec))(va_list);
+void format_int(const char *format, ...);
+
+
 
 
 
